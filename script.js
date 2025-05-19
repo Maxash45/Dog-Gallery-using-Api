8fb690920c79a024a7d828826c49c84bcb98a7f5
Yuvrajksutar <163873052+Maxash45@users.noreply.github.com>
2025-05-19 16:43:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,68 @@
-function newbreed(i){
-const output = document.getElementById('output')
-let req = new XMLHttpRequest();
-req.open("get", "https://api.thedogapi.com/v1/breeds")
-req.send();
+let i = 0;
 
-let y=i+10;
-console.log(y);
-req.addEventListener("load", function (event) {
-  var data = JSON.parse(event.target.responseText)
-  console.log(i)
-  $("#output").html("");
-  data.forEach(function (val) {
+function newbreed(startIndex) {
+  const output = document.getElementById('output');
+  let req = new XMLHttpRequest();
+  req.open("get", "https://api.thedogapi.com/v1/breeds");
+  req.send();
 
-    if (i < y) {
-      let new_div = document.createElement("div")
-      new_div.id="div1";
+  req.addEventListener("load", function (event) {
+    const data = JSON.parse(event.target.responseText);
+    const endIndex = Math.min(startIndex + 10, data.length);
 
-      let ulist = document.createElement("ul")
-      ulist.id="ullist"
-      new_div.appendChild(ulist)
-      let image = document.createElement("img")
-      image.setAttribute("src", data[i].image["url"])
-      image.setAttribute("height", "200px")
-      image.setAttribute("width", "200px")
-      new_div.appendChild(image)
-      let item = document.createElement("li")
-      item.innerText = data[i].name;
-      new_div.appendChild(item)
-      let item2 = document.createElement("li")
-        item2.innerText = "Life span: " + data[i].life_span;
-        new_div.appendChild(item2)
-      
-       let item4 = document.createElement("li")
+    for (let index = startIndex; index < endIndex; index++) {
+      const val = data[index];
+      const new_div = document.createElement("div");
+      new_div.id = "div1";
 
-        if(data[i].breed_group === "Working")
-        item4.setAttribute("style", "color: red");
-      else if (data[i].breed_group === "Hound") 
-      item4.setAttribute("style", "color: blue");
-        else if (data[i].breed_group === "Toy") 
-        item4.setAttribute("style", "color: purple");
-        else if (data[i].breed_group === "Non-Sporting") 
-        item4.setAttribute("style", "color: yellow");
-        else if (data[i].breed_group === "Mixed") 
-        item4.setAttribute("style", "color: brown");
-        else if (data[i].breed_group === "Terrier") 
-        item4.setAttribute("style", "color: cyan");
-        else if (data[i].breed_group === "Sporting") 
-        item4.setAttribute("style", "color: green");
-        else if (data[i].breed_group === "Herding") 
-        item4.setAttribute("style", "color: pink");
+      const ulist = document.createElement("ul");
+      ulist.id = "ullist";
+      new_div.appendChild(ulist);
 
-        item4.innerText ="Breed Group:" + data[i].breed_group
-        new_div.appendChild(item4)
+      // Check for image
+      if (val.image && val.image.url) {
+        const image = document.createElement("img");
+        image.setAttribute("src", val.image.url);
+        image.setAttribute("height", "200px");
+        image.setAttribute("width", "200px");
+        new_div.appendChild(image);
+      }
 
-        let item3 = document.createElement("li")
-        item3.innerText = data[i].temperament;
-        new_div.appendChild(item3)
-      output.appendChild(new_div)
-      i++;
+      const item = document.createElement("li");
+      item.innerText = val.name;
+      new_div.appendChild(item);
+
+      const item2 = document.createElement("li");
+      item2.innerText = "Life span: " + val.life_span;
+      new_div.appendChild(item2);
+
+      const item4 = document.createElement("li");
+      const colorMap = {
+        "Working": "red",
+        "Hound": "blue",
+        "Toy": "purple",
+        "Non-Sporting": "yellow",
+        "Mixed": "brown",
+        "Terrier": "cyan",
+        "Sporting": "green",
+        "Herding": "pink"
+      };
+      const color = colorMap[val.breed_group] || "black";
+      item4.style.color = color;
+      item4.innerText = "Breed Group: " + (val.breed_group || "Unknown");
+      new_div.appendChild(item4);
+
+      const item3 = document.createElement("li");
+      item3.innerText = val.temperament || "No temperament listed";
+      new_div.appendChild(item3);
+
+      output.appendChild(new_div);
     }
-  })
-
-
-})
+  });
 }
 
-let i=0;
-let button = document.querySelector(".butt");
-button.addEventListener("click",function(e){
-
-
+const button = document.querySelector(".butt");
+button.addEventListener("click", function (e) {
   newbreed(i);
-
-  i=i+10;
-  console.log("success");
+  i += 10;
 });
